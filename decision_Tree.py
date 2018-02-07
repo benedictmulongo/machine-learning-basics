34bce3655c22cf97b05f1d84cdc5223ca453acc0
Benedith Mulongo <benedith@kth.se>
2018-02-07 14:20:29 +0100

    Update decision_Tree.py

--- decision_Tree.py
+++ decision_Tree.py
@@ -2,6 +2,8 @@ import monkdata as m
 import dtree as tree
 import random
 import sys
+import matplotlib.pyplot as plt 
+import numpy as np 
 
 def partition(data, fraction):
     ldata = list(data)
@@ -39,6 +41,50 @@ def calculate_best(Td,Vd):
     
     print("Selected tree:")
     print(tr)
-        
-Td,Vd = partition(m.monk1,0.8)    
-calculate_best(Td,Vd)
+    print("error:")
+    print(error)
+    return error, tr
+    
+def find_max_pos(lst):
+    i = 0
+    length = len(lst)
+    current_max = lst[0]
+    index = 0
+    for x in range(length):
+        if current_max <= lst[x] :
+            current_max = lst[x]
+            index = x
+    return current_max, index      
+                 
+def calculate_bestavg(rep = 10, fraction = 0.7):
+    
+    all_test_errors = []
+    all_test_trees = [] 
+    x_axis = []
+    for i in range(0,rep):
+        Td,Vd = partition(m.monk3,fraction)   
+        error, tr = calculate_best(Td,Vd)
+        all_test_errors.append(error)
+        all_test_trees.append(tr)
+        x_axis.append(i+1)
+    
+    print(all_test_errors)
+    print(all_test_trees)
+    max, index = find_max_pos(all_test_errors)
+    print("Max : " , max)
+    print("Index : ", index)
+    plt.title('Monk1 best performance for 100 samples') 
+    plt.ylabel('percentage of corrected classification') 
+    plt.xlabel('samples')
+    plt.bar(x_axis,all_test_errors)
+    plt.show()
+    
+     
+calculate_bestavg(rep = 5)
+
+
+
+
+
+
+ 
